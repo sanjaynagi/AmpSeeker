77d00f6c0a5205366dae31a596f83a73063abf46
Sanjay Nagi <sanjay.c.nagi@gmail.com>
2024-11-30 16:23:00 +0000

    move pca to utils

--- workflow/ampseekertools.py
+++ workflow/ampseekertools.py
@@ -51,3 +51,38 @@ def read_ANN_field(vcf_file):
             anns.append(ann_value)
 
     return np.array(anns)
+
+
+def pca(geno, metadata, n_components = 3, query=None, missing_threshold=0.05):
+    """
+    Load genotype data and run PCA 
+    """
+
+    if query:
+        mask = metadata.eval(query)
+        metadata = metadata[mask]
+        geno = geno.compress(mask, axis=1)
+
+    # find segregating sites
+    print("removing any invariant and highly missing sites")
+    ac = geno.count_alleles()
+    seg = ac.is_segregating()
+    gn_seg = geno.compress(seg, axis=0)
+
+    # remove highly missing sites
+    missing_mask = gn_seg.is_missing().sum(axis=1) > gn_seg.shape[1] * missing_threshold
+    gn_seg = gn_seg.compress(~missing_mask, axis=0)
+    gn_alt = gn_seg.to_n_alt()
+    
+    coords, model = allel.pca(gn_alt, n_components=n_components)
+    # flip axes back so PC1 is same orientation in each window 
+    for i in range(n_components):
+        c = coords[:, i]
+    if np.abs(c.min()) > np.abs(c.max()):
+        coords[:, i] = c * -1
+    
+    pca_df = pd.DataFrame(coords)
+    pca_df.columns = [f"PC{pc+1}" for pc in range(n_components)]
+    pca_df = pd.concat([metadata.reset_index(drop=True), pca_df], axis=1)
+    
+    return pca_df, model
